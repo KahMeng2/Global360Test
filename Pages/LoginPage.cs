87b77001e417dd34410ee85621cf8df542fd2a5b
KahMeng2 <kahmeng.lee12@gmail.com>
2025-09-09 11:32:45 +1000

    Initial commit. Finished login page model. Working on dashboard model next

---
+++ Pages/LoginPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace Global360Test.Pages
+{
+
+    internal class LoginPage
+    {
+        private readonly IPage _page;
+        private readonly ILocator _username;
+        private readonly ILocator _password;
+        private readonly ILocator _btnLogin;
+
+        public LoginPage(IPage page)
+        {
+            _page = page;
+            _username = page.Locator("#username");
+            _password = page.Locator("#password");
+            _btnLogin = page.Locator("#submit");
+        }
+
+        public async Task Login(string username, string password)
+        {
+            await _username.FillAsync(username);
+            await _password.FillAsync(password);
+            await _btnLogin.ClickAsync();
+
+        }
+
+        public async Task GoTo()
+        {
+            await _page.GotoAsync("https://demo.snipeitapp.com/login");
+        }
+
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,58 @@
+using Global360Test.Pages;
+using Microsoft.Playwright;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Global360Test
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class Tests : PageTest
+    {
+     
+        [Test]
+        public async Task Test1()
+        {
+            LoginPage loginPage = new LoginPage(Page);
+
+            // Login 
+            await loginPage.GoTo();
+            await loginPage.Login("admin", "password");
+            await Page.WaitForURLAsync("**/");
+
+            // Asset creation - long block with hardcoded values
+            // Navigate through UI to create asset
+            await Page.ClickAsync("text=Create New");
+
+            // Wait for dropdown and click Asset
+            await Page.WaitForSelectorAsync(".dropdown-menu", new() { State = WaitForSelectorState.Visible });
+            await Page.WaitForSelectorAsync("a[href*='/hardware/create']", new() { State = WaitForSelectorState.Visible });
+
+            await Page.ClickAsync("a[href*='/hardware/create']");
+
+            // Verify we're on the create asset page
+            await Page.WaitForURLAsync("**/hardware/create");
+
+            // TODO: Build asset model and go from there
+            // Obtain asset tag from the page 
+
+            // Selects the correct model. Abstract this later 
+            // Click the Select2 container to open dropdown
+            await Page.ClickAsync("#select2-model_select_id-container");
+
+            // Wait for dropdown to appear
+            await Page.WaitForSelectorAsync(".select2-dropdown", new() { State = WaitForSelectorState.Visible });
+
+            // Type to search for MacBook Pro (Select2 usually has search)
+            await Page.TypeAsync(".select2-search__field", "MacBook Pro 13");
+
+            // Wait for and click the specific option
+            await Page.ClickAsync("text=MacBook Pro 13");
+
+            // Fill in details like model, status -> ready to deploy, then checkout to a random user (first user)
+            // Once it is done, we create an asset model, that will be used to verify later 
+
+
+
+        }
+    }
+}
